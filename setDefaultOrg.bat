2201d52fd5d19009a38dac01a0c557f9586dc991
Ashish singh chouhan <Aasheesh,saasheesh123@gmail.com>
2023-11-17 12:03:56 +0530

    Commit by schedual 17-11-2023 12:03:56.59;

--- setDefaultOrg.bat
+++ setDefaultOrg.bat
@@ -1,6 +1,1 @@
-call sf config set target-org=ASCBM
-call sf project retrieve start --manifest manifest/package.xml
-git add -A
-set commitMessage="Commit by schedual %date% %time%";
-git commit -m %commitMessage%
-git push ASCBM
+sf config set target-org=ASCBM
